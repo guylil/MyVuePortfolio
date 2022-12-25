c603f5178b0e45e178d2df0a3d6e5b3141fb2896
g <guylilian@gmail.com>
2022-12-26 01:22:36 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,72 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import router from "@/router";
+
+const items = [
+  {
+    id: 0,
+    text: "about me",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/6728c0da-6c6b-43e3-b906-18196de3b674",
+    alt: "portrait photo",
+  },
+  {
+    id: 1,
+    text: "Delorean photo",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/8f6f9929-b7bd-4b3e-9124-afc7f0693ecb",
+    alt: "Delorean photo",
+  },
+  {
+    id: 2,
+    text: "hacking...",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/5464dc2d-d986-479d-aeaf-731f58e02efa",
+    alt: "just hacking",
+  },
+  {
+    id: 3,
+    text: "Delorean in space",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/ebaef2b4-70d5-4e75-a4a3-de57385ee71e",
+    alt: "Delorean in space photos",
+  },
+  {
+    id: 4,
+    text: "Going back",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/9baec0e9-0224-4974-9de9-2c54874f0bf9",
+    alt: "Delorean and train with Doc photo",
+  },
+  {
+    id: 5,
+    text: "Cyber dragon",
+    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/0481a1ec-8b1f-4905-ab87-cff251b8f750",
+    alt: "Cyber dragon",
+  },
+];
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="bg-amber-50 mt-26">
+    <div class="flex justify-center">
+      <h1 class="text-4xl m-8">Hello my name is Guy</h1>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="flex justify-center">
+      <img
+        class="h-48 w-48 rounded-2xl"
+        src="https://lexica-serve-encoded-images2.sharif.workers.dev/sm/27fae0a8-cc73-4861-9680-225b456f7cc6"
+        alt="My Pic"
+      />
+    </div>
+    <div class="flex justify-center">
+      <div class="m-8 text-4xl">Come, see my projects...</div>
+    </div>
+    <div class="grid grid-cols-3 grid-rows-2 gap-4 place-items-center mt-4">
+      <div v-for="item in items" @click="()=>window.location.href='https://github.com/guylil/jokeMachine'">
+        <img
+          :src="item.url"
+          :alt="item.alt"
+          class="h-48 hover:scale-110 transition duration-300 cursor-pointer hover:ring-2 ring-offset-4 ring-cyan-400 rounded-2xl"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
