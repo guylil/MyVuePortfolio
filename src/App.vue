a0b63e2ee3a875af8d5ced22252b6fbda4707943
g <guylilian@gmail.com>
2023-01-09 20:05:24 +0200

    moved items to json file

--- src/App.vue
+++ src/App.vue
@@ -3,46 +3,11 @@ import { ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import router from "@/router";
 
-const items = [
-  {
-    id: 0,
-    text: "about me",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/6728c0da-6c6b-43e3-b906-18196de3b674",
-    alt: "portrait photo",
-  },
-  {
-    id: 1,
-    text: "Delorean photo",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/8f6f9929-b7bd-4b3e-9124-afc7f0693ecb",
-    alt: "Delorean photo",
-  },
-  {
-    id: 2,
-    text: "hacking...",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/5464dc2d-d986-479d-aeaf-731f58e02efa",
-    alt: "just hacking",
-  },
-  {
-    id: 3,
-    text: "Delorean in space",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/ebaef2b4-70d5-4e75-a4a3-de57385ee71e",
-    alt: "Delorean in space photos",
-  },
-  {
-    id: 4,
-    text: "Going back",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/9baec0e9-0224-4974-9de9-2c54874f0bf9",
-    alt: "Delorean and train with Doc photo",
-  },
-  {
-    id: 5,
-    text: "Cyber dragon",
-    url: "https://lexica-serve-encoded-images2.sharif.workers.dev/sm/0481a1ec-8b1f-4905-ab87-cff251b8f750",
-    alt: "Cyber dragon",
-  },
-];
+import { items } from "./items.json"
+
 let sent = ref(false);
 let emailAddress = ref(null);
+
 </script>
 
 <template>
@@ -103,4 +68,6 @@ let emailAddress = ref(null);
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
